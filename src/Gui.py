3d452262930594696551d59a9a4c904d26789d18
AndrewMDelgado <delgado.amarcus@gmail.com>
2019-04-12 01:16:56 -0500

    Developed GUI to configure game settings

--- src/Gui.py
+++ src/Gui.py
@@ -1,54 +1,130 @@
 #from tkinter import *
 #import tkinter
-from tkinter import Tk, ttk, LEFT, RIGHT, BOTTOM, TOP, BOTH, N, S, E, W, NW
+from tkinter import Tk, ttk, LEFT, RIGHT, BOTTOM, TOP, BOTH, \
+    N, S, E, W, NW, Button, Radiobutton, Label, IntVar, NORMAL, DISABLED
 import tkinter.messagebox as msgbox
 
+WHITE = True
+BLACK = False
 
 
+class GUI():
 
-def dispAIMove(moveStr):
-    msg = moveStr[:2] + ' to ' + moveStr[2:]
-    msgbox.showinfo("AI Move", msg)
+    def dispAIMove(self, moveStr):
+        msg = moveStr[:2] + ' to ' + moveStr[2:]
+        msgbox.showinfo("AI Move", msg)
 
-def getPlayerMove():
-    window = Tk()
-    window.wm_title("Player Move")
-    window.mainloop()
+    def getPlayerMove(self):
+        window = Tk()
+        window.wm_title("Player Move")
+        window.mainloop()
 
 
 def guiMain():
     winMain = Tk()
     winMain.wm_title("UTA Chess Bot")
+    winMain.config(width=480, height=270)
+    winMain.pack_propagate(False)
     winMain.resizable(False, False)
-    
-    frames = {}
-    notebook = ttk.Notebook(winMain)
-    frames['main_tab'] = ttk.Frame(notebook)
-    frames['misc_tab'] = ttk.Frame(notebook)
-    notebook.add(frames['main_tab'], text='Main Rules')
-    notebook.add(frames['misc_tab'], text='Misc Rules')
-    #frames['color_tab'] = ttk.Frame(notebook)
-    #frames['level_tab'] = ttk.Frame(notebook)
-    #frames['misc_tab'] = ttk.Frame(notebook)
-    #notebook.add(frames['color_tab'], text='Player color')
-    #notebook.add(frames['level_tab'], text='AI level')
-    #notebook.add(frames['misc_tab'], text='Other options')
-    #notebook.pack()
-    
-    frames['color'] = ttk.LabelFrame(frames['main_tab'], text='Player Color', labelanchor=NW, width = 200, height = 200)
-    frames['level'] = ttk.LabelFrame(frames['main_tab'], text='AI Level', labelanchor=NW, width = 200, height = 200)
-    frames['misc'] = ttk.LabelFrame(frames['misc_tab'], text='Misc. Rules', labelanchor=NW, width = 200, height = 200)
-    #frames['color'].pack(side = LEFT)
-    #frames['level'].pack(side = RIGHT)
-    #frames['misc'].pack(side = BOTTOM)
-
-    frames['color'].pack(fill=BOTH, expand=True, anchor=N, side=LEFT)
-    frames['level'].pack(fill=BOTH, expand=True, anchor=N, side=RIGHT)
-    frames['misc'].pack(fill=BOTH, expand=True, anchor=N, side=BOTTOM)
-    notebook.pack()
-
     winMain.mainloop()
 
+
+def guiStart():
+
+    def plSel():
+        if int(plColor.get()) == 1:
+            msg = "White goes first."
+            L1.config(state=NORMAL)
+            L2.config(state=NORMAL)
+            L3.config(state=NORMAL)
+            aiLabel.config(state=NORMAL)
+
+        elif int(plColor.get()) == 2:
+            msg = "Black goes second."
+            L1.config(state=NORMAL)
+            L2.config(state=NORMAL)
+            L3.config(state=NORMAL)
+            aiLabel.config(state=NORMAL)
+
+        elif int(plColor.get()) == 3:
+            msg = "Two-player; no AI"
+            L1.config(state=DISABLED)
+            L2.config(state=DISABLED)
+            L3.config(state=DISABLED)
+            aiLabel.config(state=DISABLED)
+        
+        plLabel.config(text=msg)
+
+    def aiSel():
+        if int(aiLevel.get()) == 1:
+            msg = "AI does not look ahead."
+        elif int(aiLevel.get()) == 2:
+            msg = "AI looks one move ahead."
+        elif int(aiLevel.get()) == 3:
+            msg = "AI looks two moves ahead."
+
+        aiLabel.config(text=msg)
+    
+    def startGame():
+        
+        winStart.destroy()
+        guiMain()
+    
+    def exitGame():
+        exit(0)
+
+    winStart = Tk()
+    winStart.wm_title("Config Game")
+    winStart.config(width=360, height=225)
+    winStart.pack_propagate(False)
+    #winStart.resizable(False, False)
+    
+    frames = {}
+    frames['options'] = ttk.Frame(winStart)
+    frames['options'].pack(side=TOP, pady=10)
+    
+    frames['color'] = ttk.LabelFrame(frames['options'], text='Player Color', labelanchor=NW, width = 175, height = 150)
+    frames['level'] = ttk.LabelFrame(frames['options'], text='AI Level', labelanchor=NW, width = 175, height = 150)
+    frames['color'].pack(fill=BOTH, expand=True, anchor=N, side=LEFT)
+    frames['level'].pack(fill=BOTH, expand=True, anchor=N, side=RIGHT)
+    frames['color'].pack_propagate(False)
+    frames['level'].pack_propagate(False)
+    
+    plColor = IntVar()
+    plColor.set(1)
+    P1 = Radiobutton(frames['color'], text="White", variable=plColor, value=1, command=plSel)
+    P2 = Radiobutton(frames['color'], text="Black", variable=plColor, value=2, command=plSel)
+    P3 = Radiobutton(frames['color'], text="Two-Player", variable=plColor, value=3, command=plSel)
+    P1.pack(anchor=W)
+    P2.pack(anchor=W)
+    P3.pack(anchor=W)
+
+    plLabel = Label(frames['color'], text="White goes first.")
+    plLabel.pack(anchor=W, pady=20)
+
+    aiLevel = IntVar()
+    aiLevel.set(2)
+    L1 = Radiobutton(frames['level'], text="Level 1", variable=aiLevel, value=1, command=aiSel)
+    L2 = Radiobutton(frames['level'], text="Level 2", variable=aiLevel, value=2, command=aiSel)
+    L3 = Radiobutton(frames['level'], text="Level 3", variable=aiLevel, value=3, command=aiSel)
+    L1.pack(anchor=W)
+    L2.pack(anchor=W)
+    L3.pack(anchor=W)
+
+    aiLabel = Label(frames['level'], text="AI looks one move ahead.")
+    aiLabel.pack(anchor=W, pady=20)
+
+    frames['start'] = ttk.Frame(winStart)
+    frames['start'].pack()
+    startButton = Button(frames['start'], text="Start Game", command=startGame)
+    startButton.pack(side=LEFT, padx = 15, pady=15)
+    exitButton = Button(frames['start'], text="Exit", command=exitGame)
+    exitButton.pack(side=RIGHT, pady=15)
+    
+    #winStart.state('zoomed')
+    winStart.mainloop()
+
+
 if __name__ == '__main__':
-    guiMain()
-    #getPlayerMove()
+    guiStart()
+    
