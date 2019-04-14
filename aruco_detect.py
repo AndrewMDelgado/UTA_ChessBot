4e84bb57fd877dec6a5618e15ad590e6f4f6e83e
lenangungu <lenangungu@hotmail.co.uk>
2019-04-14 01:15:37 -0500

    aruco_detect file added
    This file contains 2 functions called by the templateMatching file.

---
+++ aruco_detect.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+
+def detectCode():
+    
+    image = cv2.imread("start.jpeg")
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+
+    parameters = aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(
+    image, aruco_dict, parameters=parameters)
+    #print(corners, ids, rejectedImgPoints)
+    aruco.drawDetectedMarkers(image, corners, ids)
+    
+    #aruco.drawDetectedMarkers(image, rejectedImgPoints, borderColor=(100, 0, 240))
+    cv2.namedWindow('so52814747',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('so52814747',600,600)
+    cv2.imshow('so52814747', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
+    '''
+    #for i in corners:
+     #   print(i)
+    '''    
+       
+
+    return (ids,corners)
+'''
+if ids != None:
+    rvec, tvec = aruco.estimatePoseSingleMarkers(corners, 0.6, camera_matrix, dist_coeffs)
+    print(rvec,tvec)
+
+for i in range(1,len(corners)):
+    x = (corners[i-1][0][0][0] + corners[i-1][0][1][0] + corners[i-1][0][2][0] + corners[i-1][0][3][0]) / 4
+    y = (corners[i-1][0][0][1] + corners[i-1][0][1][1] + corners[i-1][0][2][1] + corners[i-1][0][3][1]) / 4
+    print(" x is: ", x, " y is: ", y)
+
+'''
+def detectCode2():
+    
+    image = cv2.imread("move2.jpeg")
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+
+    parameters = aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(
+    image, aruco_dict, parameters=parameters)
+    #print(corners, ids, rejectedImgPoints)
+    aruco.drawDetectedMarkers(image, corners, ids)
+    
+    #aruco.drawDetectedMarkers(image, rejectedImgPoints, borderColor=(100, 0, 240))
+    cv2.namedWindow('so52814747',cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('so52814747',600,600)
+    cv2.imshow('so52814747', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
+    '''
+    #for i in corners:
+     #   print(i)
+    '''    
+       
+
+    return (ids,corners)
